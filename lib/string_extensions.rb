3fdf690b8d34c2155bfee79de8dc9b320ecb9dff
Thomas Sommer <ts@Ts-MacBook.local>
2009-07-01 11:39:40 +1000

    Extended the typed method so that we can handle time values as well.

--- lib/string_extensions.rb
+++ lib/string_extensions.rb
@@ -1,11 +1,14 @@
 module StringExtensions
   def numeric?; !self.match(/\A\-?\d+(\.\d+)?\Z/).nil? end
   def date?; !self.match(/\A\d?\d\/\d?\d(\/\d{2,4})?\Z/).nil? end
+  def time?; !self.downcase.match(/\d+(:|am|pm)\d*/).nil? end
   
-  def typed
-    if numeric?;  to_f
-    elsif date?;  Chronic.parse(self.split("/").reverse.join("-")).try(:to_date)
-    else;         self.downcase
+  def typed(ref = nil)
+    case
+      when numeric?;  self.to_f
+      when date?;     Chronic.parse(self.split("/").reverse.join("-")).try(:to_date)
+      when time?;     Chronic.parse("#{ref} #{self}")
+      else            self.downcase
     end
   end
 end
--- spec/string_extensions_spec.rb
+++ spec/string_extensions_spec.rb
@@ -30,6 +30,20 @@ describe String do
     end
   end
   
+  describe "time?" do
+    %w[9:00 09:00 9am 9pm 9AM 9PM].each do |value|
+      it "should return true for '#{value}'" do
+        value.should be_time
+      end
+    end
+    
+    %w[abc 9 09 090 09\ 00 9\ am 9\ pm].each do |value|
+      it "should return false for '#{value}'" do
+        value.should_not be_time
+      end
+    end
+  end
+  
   describe "typed" do
     it "should return the float value if the value is numeric" do
       "1.1".typed.should == 1.1
@@ -42,6 +56,15 @@ describe String do
     it "should pass in the reversed date, splitted at the dashes, with hyphens" do
       Chronic.should_receive(:parse).with "2009-12-24"
       "24/12/2009".typed
+    end
+    
+    it "should parse the original value with chronic if the value is a time" do
+      "09:00".typed.should be_a(Time)
+    end
+    
+    it "should pass in the ref forwarded to typed" do
+      Chronic.should_receive(:parse).with "ref 09:00"
+      "09:00".typed "ref"
     end
     
     it "should return self if the value is neither numeric nor a date" do
